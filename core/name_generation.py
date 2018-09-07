b32eadd07222a90df2bd8001cd1bd32422d02749
steve <meadsteve@gmail.com>
2018-09-07 14:51:47 +0200

    extract out generation logic from view

---
+++ core/name_generation.py
@@ -0,0 +1,15 @@
+import random
+
+name_part_ones = ['spotted', 'toad', 'dick', 'toddy', 'beef', 'clotted', 'bangers',
+                  'ham', 'fish', 'fried', 'jellied', 'black', 'Ploughman\'s']
+name_part_twos = ['hole', 'pie', 'wellington', 'mash', 'tiddly', 'eels', 'pudding']
+joining_words = ['in the', 'and'] + [''] * 10
+suffix = ['in gravy', 'with gravy', 'with chips'] + [''] * 10
+
+
+def generate_food_name():
+    return f"{choose(name_part_ones)} {choose(joining_words)} {choose(name_part_twos)} {choose(suffix)}"
+
+
+def choose(choices):
+    return choices[random.randint(0, len(choices)-1)]
--- core/views.py
+++ core/views.py
@@ -1,23 +1,9 @@
-import random
-
 from django.http import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
 from django.template import loader
 
-
-name_part_ones = ['spotted', 'toad', 'dick', 'toddy', 'beef', 'clotted', 'bangers', 'ham', 'fish', 'fried', 'jellied', 'black']
-name_part_twos = ['hole', 'pie', 'wellington', 'mash', 'tiddly', 'eels', 'pudding']
-joining_words = ['in the', 'and'] + [''] * 10
-suffix = ['in gravy', 'with gravy', 'with chips'] + [''] * 10
-
-def generate_food_name():
-    return f"{choose(name_part_ones)} {choose(joining_words)} {choose(name_part_twos)} {choose(suffix)}"
-
-def choose(choices):
-    return choices[random.randint(0, len(choices)-1)]
-
+from core.name_generation import generate_food_name
 
 def index(request):
     context = {
