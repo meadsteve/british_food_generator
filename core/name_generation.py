3f3cdfe25488c11fda037bd0ba7f1888c6f6f285
steve <meadsteve@gmail.com>
2018-09-07 15:44:44 +0200

    tidy up food words

--- core/name_generation.py
+++ core/name_generation.py
@@ -1,17 +1,23 @@
 import random
+from typing import List, TypeVar
+
+def one_of_in_x_times(list: List[str], times: int) -> List[str]:
+    return list + ([''] * (times - 1) * len(list))
+
+
+T = TypeVar('T')
+def choose(choices: List[T]) -> T:
+    return choices[random.randint(0, len(choices)-1)]
 
 name_part_ones = ['spotted', 'toad', 'toddy', 'beef', 'clotted', 'bangers',
                   'ham', 'fish', 'fried', 'jellied', 'black', 'ploughman\'s',
                   'stottie', 'eccles', 'eaton', 'shepherd\'s', 'knickerbocker']
 name_part_twos = ['hole', 'pie', 'wellington', 'mash', 'tiddly', 'eels', 'pudding',
                   'stottie', 'pasty', 'tart', 'crumble', 'dick', 'glory']
-joining_words = ['in the', 'and'] + [''] * 5
-suffix = ['in gravy', 'with gravy', 'with chips', 'with mint sauce'] + [''] * 15
+
+joining_words = one_of_in_x_times(['in the', 'and'], times=3)
+suffix = one_of_in_x_times(['in gravy', 'with gravy', 'with chips', 'with mint sauce'], times=4)
 
 
-def generate_food_name():
+def generate_food_name() -> str:
     return f"{choose(name_part_ones)} {choose(joining_words)} {choose(name_part_twos)} {choose(suffix)}"
-
-
-def choose(choices):
-    return choices[random.randint(0, len(choices)-1)]
