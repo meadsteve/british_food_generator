3547ea55d2a512d2e445702523b03c3cc8903955
steve <meadsteve@gmail.com>
2018-09-10 15:59:49 +0200

    add permalinks

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('food/<str:encoded_food_name>/<str:encoded_description>/', views.existing, name='existing'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,16 +1,44 @@
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+
 from django.http import HttpResponse
 
-# Create your views here.
 from django.template import loader
 
 from core.description_generation import generate_food_description
 from core.name_generation import generate_food_name
 
+
+def _encode(string_data: str):
+    return urlsafe_b64encode(bytearray(string_data.encode("utf-8"))).decode("utf-8")
+
+def _decode(data):
+    return urlsafe_b64decode(data).decode("utf-8")
+
+def _build_permalink(request, food_name, description):
+    perma_link = request.build_absolute_uri(f"/food/{_encode(food_name)}/{_encode(description)}/")
+    return perma_link
+
 def index(request):
+    food_name = generate_food_name().capitalize()
+    description = generate_food_description()
+    perma_link = _build_permalink(request, food_name, description)
     context = {
-        'name': generate_food_name().capitalize(),
-        'description': generate_food_description()
+        'name': food_name,
+        'description': description,
+        'perma_link': perma_link
     }
     template = loader.get_template('british_food.html')
     return HttpResponse(template.render(context, request))
 
+
+def existing(request, encoded_food_name, encoded_description):
+    food_name = _decode(encoded_food_name)
+    description = _decode(encoded_description)
+    perma_link = _build_permalink(request, food_name, description)
+    context = {
+        'name': food_name,
+        'description': description,
+        'perma_link': perma_link
+    }
+    template = loader.get_template('british_food.html')
+    return HttpResponse(template.render(context, request))
