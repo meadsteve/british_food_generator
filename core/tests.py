3ef0a6e71e8a7f0aef34dbc4b51338ad2da9df84
steve <meadsteve@gmail.com>
2018-09-11 16:41:40 +0200

    black formatting

--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase, Client
 
+
 class SmokeTests(TestCase):
     client = Client()
 
     def test_the_minimum_possible_works(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         assert response.status_code == 200
