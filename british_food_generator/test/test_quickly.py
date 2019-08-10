cdc520c3e4da9daea50352029ffa25824caf0500
steveb <steveb@spotify.com>
2019-08-10 13:40:01 +0200

    testing!

---
+++ british_food_generator/test/test_quickly.py
@@ -0,0 +1,30 @@
+from starlette.testclient import TestClient
+
+from british_food_generator.app import app
+from british_food_generator.description_generation import generate_food_description
+from british_food_generator.name_generation import generate_food_name
+
+
+def test_food_name_is_a_string():
+    assert type(generate_food_name()) == str
+
+
+def test_food_description_is_a_string():
+    assert type(generate_food_description()) == str
+
+
+client = TestClient(app)
+
+
+def test_app_serves_some_html_at_the_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"The latest British Dishes" in response.content
+
+
+def test_app_raw_api_end_point_serves_some_json():
+    response = client.get("/raw")
+    assert response.status_code == 200
+    json = response.json()
+    assert "name" in json
+    assert "description" in json
--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 set -ex
 
 pipenv run mypy --ignore-missing-imports --strict-optional --check-untyped-defs british_food_generator/
+pipenv run pytest british_food_generator -vv
 pipenv run black --check british_food_generator
